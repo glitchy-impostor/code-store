61f6668df90759a81c2ab9c8f6d9dbb2315773b5
glitchy-impostor <75087610+glitchy-impostor@users.noreply.github.com>
2025-07-10 13:51:54 +0530

    Code modified and saved

--- generated/3abd0e53d6da498db08fefc4d81b99e1.py
+++ generated/3abd0e53d6da498db08fefc4d81b99e1.py
@@ -14,7 +14,19 @@ def execute_code(code, command_args):
         'command_args': command_args
     }
     response = requests.post(url, json=payload)
-    return response.json()
+    result = response.json()
+    
+    output_text = "Output: " + result['output'] + "\n\n"
+    output_text += "Execution ID: " + str(result['execution_id']) + "\n\n"
+    files_ = result['generated_files']
+    if(len(files_) > 0):
+        output_text += "Files Generated: \n"
+    else:
+        output_text += "Files Generated: None"
+    for file_ in files_:
+        output_text += file_ + " Located at: http://127.0.0.1:8000/download/" + result['execution_id'] + "/" + file_ + " \n"
+    
+    return output_text
 
 def send_email(subject, body, to_email):
     """Send an email via Gmail's SMTP server."""
@@ -69,20 +81,17 @@ if __name__ == "__main__":
     schedule_parser = subparsers.add_parser('schedule', help="Schedule a one-time execution of the code")
     schedule_parser.add_argument('code', type=str, help='The code string to execute')
     schedule_parser.add_argument('command_args', type=str, help='Any additional command arguments for the code')
-    schedule_parser.add_argument('schedule_time', type=int, help='Time in seconds from now to schedule the execution (between 60 and 604800 seconds)')
+    schedule_parser.add_argument('schedule_time', type=int, help='Time in seconds from now to schedule the execution (between 60 and 604800)')
 
     # Command for recurring scheduling
     recurring_parser = subparsers.add_parser('recurring', help="Schedule a recurring execution of the code")
     recurring_parser.add_argument('code', type=str, help='The code string to execute')
     recurring_parser.add_argument('command_args', type=str, help='Any additional command arguments for the code')
-    recurring_parser.add_argument('interval', type=int, help='Time in seconds between each execution')
+    recurring_parser.add_argument('interval', type=int, help='Time in seconds between each execution (between 60 and 604800)')
 
     args = parser.parse_args()
 
     if args.command == 'schedule':
-        if 60 <= args.schedule_time <= 604800:
-            driver_function(args.code, args.command_args, args.schedule_time)
-        else:
-            print("Schedule time must be between 1 minute and 7 days (inclusive).")
+        driver_function(args.code, args.command_args, args.schedule_time)
     elif args.command == 'recurring':
         recurring_driver_function(args.code, args.command_args, args.interval)
